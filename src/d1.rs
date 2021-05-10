99f02267479bcf3df58576a272aed5bd94f370aa
James Hoctor <JEHoctor@protonmail.com>
2021-05-10 19:38:53 -0400

    Use command line args to open puzzle input

--- src/d1.rs
+++ src/d1.rs
@@ -1,6 +1,27 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
 
 fn main() {
-    let _args: Vec<String> = env::args().collect();
-    println!("Hello, world!");
+    // get the path to the input from the command line
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: {} /path/to/day/one/input", args[0]);
+        std::process::exit(1);
+    }
+    let input_path = Path::new(&args[1]);
+
+    // open the file
+    let display = input_path.display();
+    let mut file = match File::open(&input_path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(_) => print!("{} contains:\n{}", display, s),
+    }
 }
