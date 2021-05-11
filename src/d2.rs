2fa1c77a43b3d26fdfa121fc353a82c2b8fb56ba
James Hoctor <JEHoctor@protonmail.com>
2021-05-10 21:16:04 -0400

    Add files for day 2

---
+++ src/d2.rs
@@ -0,0 +1,31 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+fn main() {
+    // get the path to the input from the command line
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: {} /path/to/day/two/input", args[0]);
+        std::process::exit(1);
+    }
+    let input_path = Path::new(&args[1]);
+
+    // open the file
+    let display = input_path.display();
+    let mut file = match File::open(&input_path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    let mut input = String::new();
+    match file.read_to_string(&mut input) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(_) => (),
+    }
+
+    // println!("part 1: {}", );
+
+    // println!("part 2: {}", );
+}
