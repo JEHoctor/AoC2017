d7afc50baf77b33ecdd585ff1f086c9c18f67a69
James Hoctor <JEHoctor@protonmail.com>
2021-05-21 17:04:12 -0400

    Day 2 part 2 solution

--- src/d2.rs
+++ src/d2.rs
@@ -3,6 +3,21 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 
+fn p2_row(nums: &mut Vec<i32>) -> i32 {
+    nums.sort();
+
+    for (i, n1) in nums.iter().enumerate() {
+        for n2 in nums[i+1..].iter() {
+            if n2 % n1 == 0 {
+                return n2 / n1;
+            }
+        }
+    }
+
+    panic!("did not find a pair of numbers such that one divides the other");
+}
+
+
 fn main() {
     // get the path to the input from the command line
     let args: Vec<String> = env::args().collect();
@@ -26,12 +41,18 @@ fn main() {
     }
 
     // state variables for parsing the block of numbers
-    let mut acc = 0;
-    let mut max_sf = 0;
-    let mut min_sf = 0;
     let mut row_init = false;
     let mut word_start = 0;
     let mut in_word = false;
+
+    // state variables for computing part 1
+    let mut acc = 0;
+    let mut max_sf = 0;
+    let mut min_sf = 0;
+
+    // state variables for computing part 2
+    let mut acc2 = 0;
+    let mut nums = Vec::new();
 
     for (i, c) in input.chars().enumerate() {
         match c {
@@ -47,12 +68,15 @@ fn main() {
                     } else {
                         max_sf = value;
                         min_sf = value;
+                        nums.clear();
                         row_init = true;
                     }
+                    nums.push(value);
                 }
                 in_word = false;
                 if (c == '\n') & row_init {
                     acc += max_sf - min_sf;
+                    acc2 += p2_row(&mut nums);
                     row_init = false;
                 }
             },
@@ -67,5 +91,5 @@ fn main() {
 
     println!("part 1: {}", acc);
 
-    // println!("part 2: {}", );
+    println!("part 2: {}", acc2);
 }
