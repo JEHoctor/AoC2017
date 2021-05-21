01b972cc7714f8926052952034b15c89545d7f76
James Hoctor <JEHoctor@protonmail.com>
2021-05-21 16:11:09 -0400

    Day 2 part 1 solution

--- src/d2.rs
+++ src/d2.rs
@@ -25,7 +25,47 @@ fn main() {
         Ok(_) => (),
     }
 
-    // println!("part 1: {}", );
+    // state variables for parsing the block of numbers
+    let mut acc = 0;
+    let mut max_sf = 0;
+    let mut min_sf = 0;
+    let mut row_init = false;
+    let mut word_start = 0;
+    let mut in_word = false;
+
+    for (i, c) in input.chars().enumerate() {
+        match c {
+            '\n' | '\t' => {
+                if in_word {
+                    let value = (&input[word_start..i]).parse::<i32>().unwrap();
+                    if row_init {
+                        if value > max_sf {
+                            max_sf = value;
+                        } else if value < min_sf {
+                            min_sf = value;
+                        }
+                    } else {
+                        max_sf = value;
+                        min_sf = value;
+                        row_init = true;
+                    }
+                }
+                in_word = false;
+                if (c == '\n') & row_init {
+                    acc += max_sf - min_sf;
+                    row_init = false;
+                }
+            },
+            _ => {
+                if !in_word {
+                    in_word = true;
+                    word_start = i;
+                }
+            },
+        }
+    }
+
+    println!("part 1: {}", acc);
 
     // println!("part 2: {}", );
 }
